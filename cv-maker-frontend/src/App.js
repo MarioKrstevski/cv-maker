6f153f2becada3fa0207a9ac4224deb08b42f35e
Mario Krstevski <Mario.Krstevski@netcetera.com>
2019-03-16 00:16:11 +0100

    Transitioned MuiThemeProvider import in another file

--- cv-maker-frontend/src/App.js
+++ cv-maker-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.scss';
 
 class App extends Component {
