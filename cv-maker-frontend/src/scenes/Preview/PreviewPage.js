ce41f0649deb972f753cef2307c11cd492ec1c93
Mario Krstevski <mariokrstevski@hotmail.com>
2019-04-21 11:37:21 +0200

    [PreviewPage] Personal info styled

--- cv-maker-frontend/src/scenes/Preview/PreviewPage.js
+++ cv-maker-frontend/src/scenes/Preview/PreviewPage.js
@@ -3,10 +3,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PersonalInfo = styled.div`
-display: grid;
-grid-template-columns: 1fr 1fr;
-align-items:center;
-
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  align-items:center;
   height: 160px;
   border: 1px solid purple;
   background-color: rgba(102,51,153, 0.2);
@@ -44,12 +43,13 @@ const SoftSkills = styled.div``;
 const Languages = styled.div``;
 const Interests = styled.div``;
 const ProfilePhoto = styled.img`
-grid-area: imgPhoto;
 
- object-fit: cover;
- object-position:center;
- width:130px;
- height:130px;
+  margin-top: 13px;
+  border: 1px solid black;
+  object-fit: cover;
+  object-position:center;
+  width:130px;
+  height:130px;
     border-radius: 50%;
    
 `;
@@ -68,21 +68,31 @@ font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubun
   }
 `;
 const ImgAndInfo = styled.div`
-display: grid;
-grid-template-areas:
-    'imgPhoto name'
-    'imgPhoto empty'
-    'imgPhoto position'
-`;
+  display: flex;
+  margin-left:14px;
+`; 
 const Links = styled.div`
+margin-right: 14px;
+text-align:right;
+   & > *{
+      font-weight:500;
+      padding: 0;
+      margin: 0;
+   }
 `;
-const Name = styled.div`
-  grid-area: name;
-
+const NameAndPosition = styled.div`
+margin-top: 13px;
+margin-left:2px;
+display: inline-block;
+border: 1px solid grey;
+width: calc(100% - 170px);
+height: 150px;
+`;
+const Name = styled.h1`
+  margin-bottom: 5px;
 `;
 const Position = styled.div`
-  grid-area: position;
-
+  font-size:19px;
 `;
 
 export default function PreviewPage() {
@@ -90,27 +100,30 @@ export default function PreviewPage() {
     <PreviewWrapper>
       <PersonalInfo> 
         <ImgAndInfo> 
-            <ProfilePhoto src="https://jamesgilberdphotography.weebly.com/uploads/1/3/6/5/13650410/emma-not-smiling-35x45_1_orig.jpg" />
+        <div style={{width: 150, height: 157, display:'inline-block', border: '1px solid red'}}>
+          <ProfilePhoto src="https://jamesgilberdphotography.weebly.com/uploads/1/3/6/5/13650410/emma-not-smiling-35x45_1_orig.jpg" />
+        </div>
+          <NameAndPosition>
             <Name> Suzanne Anotonie </Name>
             <Position> Hiring Manager </Position>
+          </NameAndPosition>
         </ImgAndInfo>
         <Links> 
-            <p> mail </p>
-            <p> things </p>
-            <p> small </p>
-            <p> shhh </p>
-            <p> voids </p>
+          <p> johndoe@gmail.com </p>
+          <p> 070-445-556 </p>
+          <p> Skopje, Macedonia </p>
+          <p> linkedin.com/in/suzzane-antonie </p>
+          <p> facebook.com/SuzzaneAnt </p>
         </Links>
       </PersonalInfo>
       <Introduction>
-      A passionate, young and highly motivated developer who is focused on Front End technologies. 
-      I'm always looking forward to take on a new project/opportunity where I can use and improve my skills. 
-      I love to create, contribute and meet new people, therefore I tend to volunteer wherever I can around me. I аm looking for a challenging internship opportunity where i can improve even further.
+        A passionate, young and highly motivated developer who is focused on Front End technologies. 
+        I'm always looking forward to take on a new project/opportunity where I can use and improve my skills. 
+        I love to create, contribute and meet new people, therefore I tend to volunteer wherever I can around me. I аm looking for a challenging internship opportunity where i can improve even further.
        </Introduction>
       <DetailInformation>
         <Education />
         <WorkExperience />
-
         <SideInfo />
       </DetailInformation>
     </PreviewWrapper>
